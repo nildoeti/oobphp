4354897d9cef434889696c5d5861dbbbe5c914ae
Nildo <nildo@webstack.com.br>
2022-10-23 15:58:54 -0300

    script para switch finalizado

--- capitulo-1/switch-1.php
+++ capitulo-1/switch-1.php
@@ -6,18 +6,37 @@
 	*/
 	$i = 1;
 
-	if ($i == 0) {
-		print 'I é igual a 0';
+	// exemplo 2
+	switch ($i) {
+		case 0:
+			print 'I é igual a 0';
+			break;
+		case 1:
+			print 'I é igual a 1';
+			break;
+		case 2:
+			print 'I é igual a 2';
+			break;
+		default:
+			print 'I não é igual a 0, 1 e 2';
+
 	}
-	elseif ($i == 1) {
-		print 'I é igual 1';
-	}
-	elseif ($i == 3) {
-		print "i é igual a 3";
-	}
-	else {
-		print "I não é igual a 0, 1 e 2";
-	}
+
+
+	// exemplo 1
+	// if ($i == 0) {
+	// 	print 'I é igual a 0';
+	// }
+	// elseif ($i == 1) {
+	// 	print 'I é igual 1';
+	// }
+	// elseif ($i == 3) {
+	// 	print "i é igual a 3";
+	// }
+	// else {
+	// 	print "I não é igual a 0, 1 e 2";
+	// }
+
 
 
 
