e4989f785cdfc3dd6abd1fa9dda271b96c788cd9
Nildo <nildo@webstack.com.br>
2022-11-13 19:50:26 -0300

    finalizado 4 exemplos de uso para o laço for

--- capitulo-1/for-2.php
+++ capitulo-1/for-2.php
@@ -1,6 +1,8 @@
-<?php 
+<?php
 	// considerar revisar o código quando utilizando 3 ou mais nível encadeados
 	// segundo Linux Torvalds
+
+	// exemplo 1
 	for ($i = 0; $i < 10; $i++)
 	{
 		for ($j = 0; $j < 9; $j++)
@@ -11,4 +13,40 @@
 			}
 		}
 	}
-?>
+
+	// exemplo 1
+	for($i = 0; $i <= 10; $i++) {
+		// echo $i;
+	}
+
+	// exemplo 2
+	for ($i = 0; ; $i++) {
+		if ($i > 10) {
+			break;
+		}
+		// echo $i;
+	}
+
+	// exemplo 3
+	$i = 1;
+	for (;;) {
+		if ($i > 10) {
+			break;
+		}
+		// echo $i;
+		$i++;
+	}
+
+	// exemplo 4
+/* exemplo 4 */
+$n = 1;
+for (;;) {
+	if ($n > 10) {
+		break;
+	}
+	// echo $n;
+	$n++;
+}
+
+// exemplo 5
+for ($m = 1, $n = 0; $m < 100; $n += $m, print $m, $m++);
